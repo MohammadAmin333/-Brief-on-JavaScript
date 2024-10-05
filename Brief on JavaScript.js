b0e689748176eedda0fac0cfd0a2eea8c59def74
MohammadAmin333 <codmin333@gmail.com>
2024-10-05 22:57:55 +0600

    Add files via upload

---
+++ Brief on JavaScript.js
@@ -0,0 +1,20 @@
+let marks=85
+
+if (marks>=90){
+    console.log("Grade : A+");
+}
+if (marks >= 80) {
+    console.log("Grade : A");
+}
+if (marks >= 70) {
+    console.log("Grade : B");
+}
+if (marks >= 60) {
+    console.log("Grade : C");
+}
+if (marks >= 50) {
+    console.log("Grade : D");
+}
+
+
+
